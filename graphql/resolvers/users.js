ccf2274cc017c481e9053296117f08c7308f9597
tflunk <45830707+taydougherty@users.noreply.github.com>
2020-11-08 17:35:57 -0800

    added user schema and register user mutation

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,11 @@
+const User = require("../../models/User");
+
+module.exports = {
+  Mutation: {
+    register(_, args, context, info) {
+      // TODO: Validate user data
+      // TODO: Make sure user doesn't already exist
+      // TODO: Hash password and create an auth token
+    },
+  },
+};
